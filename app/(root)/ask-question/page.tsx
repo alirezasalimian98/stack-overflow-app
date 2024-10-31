35f6717a72705bd83fe474cfcade787fbcb5ff7a
Alireza Salimiyan <alirezasalimiyan98@gmail.com>
2024-10-31 19:21:17 +0330

    adding the ask question component and adding form and tynyMCE editor to the form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Question } from "@/components/form/Question";
 
 const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+  return <Question />;
 };
 
 export default AskQuestion;
