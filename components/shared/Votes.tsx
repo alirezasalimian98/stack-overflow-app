26b597f489f1786cda12492b02edc3be20c063f7
Alireza Salimiyan <alirezasalimiyan98@gmail.com>
2025-02-02 18:04:58 +0330

    adding the interaction for view count

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import { viewQuestion } from "@/lib/actions/interaction.action";
 import {
   downvoteQuestion,
   upvoteQuestion,
@@ -8,7 +9,7 @@ import { ToggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 interface Props {
   type: string;
@@ -90,6 +91,13 @@ const Votes = ({
       path: pathName,
     });
   }
+
+  useEffect(() => {
+    viewQuestion({
+      questionId: JSON.parse(itemId),
+      userId: userId ? JSON.parse(userId) : undefined,
+    });
+  }, [itemId, userId, pathName, router]);
   return (
     <div className="flex gap-5">
       <div className="flex-center gap-2.5">
